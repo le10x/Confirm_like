a744fb32375a3207c9f129709835ef4cc176d7cd
le10x <leanderdrb@gmail.com>
2026-04-23 16:33:21 -0400

    Modify like and dislike handling in MyLikeItemLayer

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,58 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/InfoLayer.hpp>
+#include <Geode/modify/LikeItemLayer.hpp>
 
 using namespace geode::prelude;
 
-class $modify(MyInfoLayer, InfoLayer) {
-    // Usamos la firma exacta que espera el compilador para evitar el error de 'no member'
+class $modify(MyLikeItemLayer, LikeItemLayer) {
+    // Definimos Fields para evitar el error de recursión infinita
+    struct Fields {
+        bool m_isConfirming = false;
+    };
+
     void onLike(CCObject* sender) {
-        int tag = sender->getTag();
-        bool isLike = (tag == 1);
-
-        // Si el tag es de confirmación (69 o 70), ejecutamos la función original directamente
-        if (tag == 69 || tag == 70) {
-            sender->setTag(tag == 69 ? 1 : 0);
-            return InfoLayer::onLike(sender);
+        // Si el ajuste está apagado o ya confirmamos, ejecutamos original
+        if (!Mod::get()->getSettingValue<bool>("confirm-like") || m_fields->m_isConfirming) {
+            m_fields->m_isConfirming = false;
+            return LikeItemLayer::onLike(sender);
         }
 
-        // Leer configuraciones
-        bool shouldConfirmLike = Mod::get()->getSettingValue<bool>("confirm-like");
-        bool shouldConfirmDislike = Mod::get()->getSettingValue<bool>("confirm-dislike");
+        // Marcamos que estamos en proceso de confirmación
+        m_fields->m_isConfirming = true;
 
-        if ((isLike && shouldConfirmLike) || (!isLike && shouldConfirmDislike)) {
-            std::string action = isLike ? "like" : "dislike";
-            
-            auto alert = FLAlertLayer::create(
-                this, 
-                "Confirm", 
-                "Are you sure you want to " + action + "?", 
-                "Cancel", 
-                "Yes"
-            );
-            
-            alert->setTag(isLike ? 69 : 70);
-            alert->show();
-        } else {
-            // Si no hay confirmación, llamar a la original
-            InfoLayer::onLike(sender);
-        }
+        geode::createQuickPopup(
+            "Confirm", 
+            "Are you sure you want to like?", 
+            "Cancel", "Yes",
+            [this, sender](auto, bool btn2) {
+                if (btn2) {
+                    this->onLike(sender);
+                } else {
+                    // Si cancela, reseteamos la bandera
+                    m_fields->m_isConfirming = false;
+                }
+            }
+        );
     }
 
-    // Manejador del botón de la alerta
-    void FLAlert_Clicked(FLAlertLayer* alert, bool btn2) {
-        if (btn2) {
-            auto dummy = CCNode::create();
-            dummy->setTag(alert->getTag());
-            this->onLike(dummy);
+    void onDislike(CCObject* sender) {
+        if (!Mod::get()->getSettingValue<bool>("confirm-dislike") || m_fields->m_isConfirming) {
+            m_fields->m_isConfirming = false;
+            return LikeItemLayer::onDislike(sender);
         }
+
+        m_fields->m_isConfirming = true;
+
+        geode::createQuickPopup(
+            "Confirm", 
+            "Are you sure you want to dislike?", 
+            "Cancel", "Yes",
+            [this, sender](auto, bool btn2) {
+                if (btn2) {
+                    this->onDislike(sender);
+                } else {
+                    m_fields->m_isConfirming = false;
+                }
+            }
+        );
     }
 };
